2b0ede3c48e3aa13bcfc2aacff418ace04c1bf32
Kevin Riquelme <kriquelme10@gmail.com>
2024-06-19 18:33:49 -0300

    Diseño al Hero y nuestros servicios

---
+++ js/script-reload.js
@@ -0,0 +1,14 @@
+let lastScrollTop = 0;
+const navegation = document.querySelector('.page-header');
+const scrollY =  window.scrollY;
+
+
+window.addEventListener('scroll', ()=> {
+      const scrollY =  window.scrollY;
+      console.log(scrollY);
+})
+
+
+// if (scrollY > 250px) {
+//       navegation.classList.add('')
+// }
