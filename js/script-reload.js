0bea521bb1f72aff1aa87e6ddb2a1d1e5c448be6
Kevin Riquelme <kriquelme10@gmail.com>
2024-07-03 00:03:42 -0300

    text hero p1 in progress OK

--- js/script-reload.js
+++ js/script-reload.js
@@ -19,3 +19,14 @@ const toggleMenu = document.getElementById('toggle-menu');
 buttonToggleMenu.addEventListener('click', function() {
       toggleMenu.classList.toggle('no-hidden')
 });
+
+
+
+
+//Animacion h1 Hero
+
+const textHeroOne = document.querySelector('.selector-text-hero');
+setTimeout(()=>{
+      textHeroOne.classList.remove('text-hero-h1__p1');
+      textHeroOne.classList.add('text-hero-h1-p__infinit');
+}, 10000);
