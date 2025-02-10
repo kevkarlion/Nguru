2d4c667f33d89bb75348eb42a721e9fd39690087
Kevin Riquelme <kriquelme10@gmail.com>
2025-02-10 15:52:36 -0300

    modify section about us

--- js/script-reload.js
+++ js/script-reload.js
@@ -23,36 +23,36 @@ buttonToggleMenu.addEventListener('click', function() {
 
 
 //Button-SABER MAS
-const containerBtnSaberMas = document.querySelector('.container__saber-mas');
-const articleSaberMas = document.querySelector('#id-article-saber-mas');
-const btnSaberMasText = document.querySelector('#button__saber-mas');
+// const containerBtnSaberMas = document.querySelector('.container__saber-mas');
+// const articleSaberMas = document.querySelector('#id-article-saber-mas');
+// const btnSaberMasText = document.querySelector('#button__saber-mas');
 
-function ajustarSeccionSegunPantalla() {
-    if (window.innerWidth > 1024) {
-        articleSaberMas.classList.add('article-saber-mas__down');
-        articleSaberMas.classList.remove('article-saber-mas__inactive');
-        containerBtnSaberMas.style.display = 'none';
-    } else {
-        articleSaberMas.classList.remove('article-saber-mas__down');
-        articleSaberMas.classList.add('article-saber-mas__inactive');
-        containerBtnSaberMas.style.display = 'flex';
-        btnSaberMasText.innerText = 'Saber más';
-    }
-}
+// function ajustarSeccionSegunPantalla() {
+//     if (window.innerWidth > 1024) {
+//         articleSaberMas.classList.add('article-saber-mas__down');
+//         articleSaberMas.classList.remove('article-saber-mas__inactive');
+//         containerBtnSaberMas.style.display = 'none';
+//     } else {
+//         articleSaberMas.classList.remove('article-saber-mas__down');
+//         articleSaberMas.classList.add('article-saber-mas__inactive');
+//         containerBtnSaberMas.style.display = 'flex';
+//         btnSaberMasText.innerText = 'Saber más';
+//     }
+// }
 
-window.addEventListener('resize', ajustarSeccionSegunPantalla);
-ajustarSeccionSegunPantalla();
+// window.addEventListener('resize', ajustarSeccionSegunPantalla);
+// ajustarSeccionSegunPantalla();
 
-// Función para alternar el contenido
-const toggleContent = (event) => {
-    if (window.innerWidth <= 1024) {
-        articleSaberMas.classList.toggle('article-saber-mas__down');
-        articleSaberMas.classList.toggle('article-saber-mas__inactive');
+// // Función para alternar el contenido
+// const toggleContent = (event) => {
+//     if (window.innerWidth <= 1024) {
+//         articleSaberMas.classList.toggle('article-saber-mas__down');
+//         articleSaberMas.classList.toggle('article-saber-mas__inactive');
 
-        btnSaberMasText.innerText = articleSaberMas.classList.contains('article-saber-mas__down') 
-            ? 'Ocultar' 
-            : 'Saber más';
-    }
-};
-containerBtnSaberMas.addEventListener('touchstart', toggleContent);
+//         btnSaberMasText.innerText = articleSaberMas.classList.contains('article-saber-mas__down') 
+//             ? 'Ocultar' 
+//             : 'Saber más';
+//     }
+// };
+// containerBtnSaberMas.addEventListener('touchstart', toggleContent);
 //----------------
